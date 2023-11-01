8c712f2fa70d514167350b4fe3170a2bd908cd47
eugeniarossi <eugenia.rossi@outlook.com>
2023-11-01 23:44:46 +0100

    feat: meme controller index & store

--- app/Http/Controllers/MemeController.php
+++ app/Http/Controllers/MemeController.php
@@ -2,9 +2,95 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Meme;
 use Illuminate\Http\Request;
 
 class MemeController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $memes = Meme::select('url', 'score')->get();
+
+        return response()->json($memes);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        // Verifica se 'preview' è un array e se sì, convertilo in una stringa JSON
+        if (is_array($data['preview'])) {
+            $data['preview'] = json_encode($data['preview']);
+        }
+
+        $newMeme = Meme::create($data);
+
+        return response()->json(['message' => 'Meme creato con successo'], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 }
--- app/Models/Meme.php
+++ app/Models/Meme.php
@@ -2,10 +2,20 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Meme extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'author',
+        'nsfw',
+        'postLink',
+        'preview',
+        'score',
+        'spoiler',
+        'subreddit',
+        'title',
+        'ups',
+        'url',
+    ];
 }
